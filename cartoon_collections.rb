77480bce90c6a263392602d7b7ec7cf3c134fe5e
Julianne Kim <juliannne@gmail.com>
2017-10-19 01:48:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(veggies)
   veggies.map { |veg| veg.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.all? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
